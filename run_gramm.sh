3ea13e16a89aeda393ef9087eca652e62e57fd52
ombhilare999 <ombhilare999@pop-os.localdomain>
2024-08-06 03:55:18 -0400

    Script modified as device model is generated for the Riken architecture with pins enabled

--- run_gramm.sh
+++ run_gramm.sh
@@ -4,11 +4,12 @@
 #Seed is default to 0
 # $1 : Kernels/Conv_Balance/conv_nounroll_Balance.dot 
 # device model will be for NR=NC=8 :: scripts/riken_8_8.dot
+Arch=RIKEN_with_pins #set to RIKEN for generating without pins
 
 # Generating device model:
 echo "---------------------Generating device model using external script:---------------------"
-echo "cd scripts && ./device_model_gen.py -NR $2 -NC $3 -Arch RIKEN && cd .."
-cd scripts && ./device_model_gen.py -NR $2 -NC $3 -Arch RIKEN && cd ..
+echo "cd scripts && ./device_model_gen.py -NR $2 -NC $3 -Arch RIKEN_with_pins && cd .."
+cd scripts && ./device_model_gen.py -NR $2 -NC $3 -Arch RIKEN_with_pins && cd ..
 
 # Executes GRAMM and producing mapping result in mapping_output.dot
 echo "---------------------Executing GRAMM and producing mapping result in mapping_output.dot---------------------"
